4e33ad2ae76968916ad7023d5b017a50d2eddd47
Higor-ViniciusDev <higor.vinicius331@hotmail.com>
2026-04-27 21:06:47 -0300

    retirando logger de info

--- internal/usecase/work/get_work.go
+++ internal/usecase/work/get_work.go
@@ -2,14 +2,11 @@ package work_usecase
 
 import (
 	"context"
-
-	"github.com/Higor-ViniciusDev/agent-ia-go/pkg/logger"
 )
 
 func (uc *workUseCase) FindByID(ctx context.Context, id string) (*WorkOutput, error) {
 	workEntity, err := uc.repo.GetByID(ctx, id)
 
-	logger.Info(id)
 	return &WorkOutput{
 		ID:     workEntity.ID,
 		Status: string(workEntity.Status),
